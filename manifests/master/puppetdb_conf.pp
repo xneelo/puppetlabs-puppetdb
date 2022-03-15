d421f05de7ad61a6dc0ca03aabbd5722ab23a563
Johan Basson <jb@xneelo.com>
2022-03-15 19:34:07 +0200

    test

--- manifests/master/puppetdb_conf.pp
+++ manifests/master/puppetdb_conf.pp
@@ -1,7 +1,7 @@
 # Manage the puppetdb.conf file on the puppeet master. See README.md for more
 # details.
 class puppetdb::master::puppetdb_conf (
-  $servers            = ['localhost'],
+  $servers            = 'localhost',
   $port               = '8081',
   $soft_write_failure = $puppetdb::disable_ssl ? {
     true => true,
