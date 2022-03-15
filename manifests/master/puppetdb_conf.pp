dc48c50c7a728f7c4ae555ae25af5517f3bd62eb
Johan Basson <jb@xneelo.com>
2022-03-15 15:58:56 +0200

    first draft

--- manifests/master/puppetdb_conf.pp
+++ manifests/master/puppetdb_conf.pp
@@ -1,7 +1,7 @@
 # Manage the puppetdb.conf file on the puppeet master. See README.md for more
 # details.
 class puppetdb::master::puppetdb_conf (
-  $server             = 'localhost',
+  $servers            = ['localhost'],
   $port               = '8081',
   $soft_write_failure = $puppetdb::disable_ssl ? {
     true => true,
@@ -30,9 +30,10 @@ class puppetdb::master::puppetdb_conf (
       value   => $port,
     }
   } else {
+    $servers_url_string = servers.each { |server| server.insert(0, 'https://') << ":#{port}"}.join(',')
     ini_setting { 'puppetdbserver_urls':
       setting => 'server_urls',
-      value   => "https://${server}:${port}/",
+      value   => $servers_url_string,
     }
   }
 
