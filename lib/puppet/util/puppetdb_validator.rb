c5b3781d4d93159c86126d7e923507586205d844
Johan Basson <jb@xneelo.com>
2022-03-15 20:39:02 +0200

    there is an old and a new client

--- lib/puppet/util/puppetdb_validator.rb
+++ lib/puppet/util/puppetdb_validator.rb
@@ -39,13 +39,15 @@ class Puppet::Util::PuppetdbValidator
   end
 
   def valid_connection_old_client?
-    conn = Puppet::Network::HttpPool.http_instance(puppetdb_server, puppetdb_port, use_ssl)
-    response = conn.get(test_path, test_headers)
-    unless response.is_a?(Net::HTTPSuccess)
-      log_error(response.msg, response.code)
-      return false
-    end
-    true
+    puppetdb_server.each { | server |
+      conn = Puppet::Network::HttpPool.http_instance(server, puppetdb_port, use_ssl)
+      response = conn.get(test_path, test_headers)
+      unless response.is_a?(Net::HTTPSuccess)
+        log_error(response.msg, response.code)
+        return false
+      end
+      true
+    }
   end
 
   # Utility method; attempts to make an http/https connection to the puppetdb server.
